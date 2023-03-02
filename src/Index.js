c100f9a17d81768331fdeaa637ff903bd5c7e479
Muhammed Rashid <rashidktr07@gmail.com>
2023-03-02 12:11:46 +0530

    complete rolex page action

--- src/Index.js
+++ src/Index.js
@@ -6,10 +6,33 @@ let imageSec=document.querySelector('.rolex-logo')
 
 navbar.addEventListener('mouseenter',()=>{
     navbar.classList.add('bg-white');
-    imageSec.src=
-    for(let i=0;i<links.length;i++){
-        console.log(links[i]);
+    imageSec.src='../Assets/LogoGreen.png' 
+    for(let i=0;i<links.length;i++){        
         links[i].classList.remove('text-white')
         links[i].classList.add('text-[rgba(18,120,80,1)]','font-bold');
     }
 })
+navbar.addEventListener('mouseleave',()=>{
+    navbar.classList.remove('bg-white');
+    imageSec.src='../Assets/LogoWhite.png';
+    for(let i=0;i<links.length;i++){        
+        links[i].classList.add('text-white')
+        links[i].classList.remove('text-[rgba(18,120,80,1)]','font-bold');
+    }
+})
+
+
+
+//scroll carousel in watches
+
+let leftButton=document.querySelector('.left-button');
+let rightButton=document.querySelector('.right-button');
+let scrollDiv=document.querySelector('.product-list');
+
+leftButton.addEventListener('click',()=>{
+    scrollDiv.scrollLeft=scrollDiv.scrollLeft-100;
+});
+rightButton.addEventListener('click',()=>{
+    scrollDiv.scrollLeft=scrollDiv.scrollLeft+100;
+})
+
